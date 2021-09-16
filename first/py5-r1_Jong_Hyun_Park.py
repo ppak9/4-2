baf8677e1a35beedcf5956c9f32b4d670445e4f3
ppak9 <petepp@naver.com>
2021-09-16 18:59:08 +0900

    modified file name

--- first/py5-r1_Jong_Hyun_Park.py
+++ first/py5-r1_Jong_Hyun_Park.py
@@ -1,15 +1,29 @@
+"""
+    1. Programmer : Jong Hyun Park
+    2. Date : 2021.09.16
+    3. File_name : py5-r1_Jong_Hyun_Park
+    4. Description : Hexagon + Circle
+
+"""
 import turtle
-import time
+# for pause final result import python module
+import time 
+
 
 # Start a work Screen
-ws = turtle.Screen()
+ws = turtle.Screen() 
  
 # Define a Turtle Instance
 t = turtle.Turtle()
+# for export final result, export turtle library of the getscreen
 ts = turtle.getscreen()
 
+# set the color list
 colors = ['red','blue','green','pink','orange','purple']
+#hiding figure
 t.ht()
+
+# for set start position
 t.penup()
 t.setpos(-100,200)
 t.pendown()
@@ -19,7 +33,9 @@ for i in range(6):
  
     # Move forward by 200 units
     t.color(colors[i])
+    # use begin_fill function
     t.begin_fill()
+    # forward 300 and backward 100 
     t.fd(300)
     t.circle(50)
     t.bk(100)
@@ -27,6 +43,8 @@ for i in range(6):
     t.right(60)
     t.end_fill()
 
+# pause the run of the turtle, for check about the result
 time.sleep(3)
+ 
+#  export final result of the picture
 ts.getcanvas().postscript(file="result.eps")
-
